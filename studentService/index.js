30d6b632539acfe7519785e2a0c894862c937582
Josh8al <josh8al@gmail.com>
2026-02-05 13:21:51 +0100

    Updated student service with rate limiter

--- studentService/index.js
+++ studentService/index.js
@@ -6,6 +6,8 @@ dotenv.config();
 const connectDB = require("./config/db");
 const studentRoutes = require("./routes/studentRoute");
 
+const { correlationIdMiddleware } = require("../correlationId");
+
 // Load environment variables from .env file
 dotenv.config();
 
@@ -17,11 +19,12 @@ connectDB();
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(correlationIdMiddleware);
 
 // Routes
 app.use("/api/students", studentRoutes);
 
 const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => {
-    console.log(`Student Service is running on port ${PORT}`);
-})
+	console.log(`Student Service is running on port ${PORT}`);
+});
